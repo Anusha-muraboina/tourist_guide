0e809b5b7d631748410535f4cdd2620f15b3380c
anushamuraboina99 <anushamuraboina99@gmail.com>
2026-09-16 09:07:52 +0530

    updated the changes
    updated the changes

--- tourist_guide/blog/urls.py
+++ tourist_guide/blog/urls.py
@@ -7,24 +7,7 @@ from .views import (
     BlogDetailAPIView,
 )
 urlpatterns = [
-    # path(
-    #     "terms/",
-    #     terms,
-    #     name="terms"
-    # ),
 
-    # path(
-    #     "policy/",
-    #     policy,
-    #     name="policy"
-    # ),
-    
-    #     path(
-    #     "about-us/",
-    #     views.about_us,
-    #     name="about_us"
-    # ),
-        
 
     path("api/blogs/", BlogListAPIView.as_view(), name="blog_list_api"),
     path("blogs/<slug:slug>/", BlogDetailAPIView.as_view(), name="blog_detail_api"),
@@ -34,29 +17,6 @@ urlpatterns = [
     path("<slug:slug>/", views.blog_detail, name="blog_detail"),
 
         
-#     path(
-#         "api/blogs/",
-#         BlogListAPIView.as_view(),
-#         name="blog_list_api"
-#     ),
-
-#     path(
-#         "blogs/<slug:slug>/",
-#         BlogDetailAPIView.as_view(),
-#         name="blog-detail"
-#     ),
-
-#     path(
-#         "blog_list/",
-#         views.blog_list,
-#         name="blog_list"
-#     ),
-    
-#     path(
-#     "blog_detail/<slug:slug>/",
-#     views.blog_detail,
-#     name="blog_detail"
-# ),
     
     
 ]
